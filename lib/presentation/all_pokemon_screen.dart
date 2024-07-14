08765854af578550c389a7ab4aae2d9273f5772f
rikuyu <rikuyu5884@gmail.com>
2024-07-14 17:07:16 +0900

    add all pokemon screen

---
+++ lib/presentation/all_pokemon_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AllPokemonsScreen extends StatelessWidget {
+  const AllPokemonsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.expand();
+  }
+}

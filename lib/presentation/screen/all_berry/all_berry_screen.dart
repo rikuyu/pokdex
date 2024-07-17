26a9e52fe5d9de608a8744587109ad8048cd397c
rikuyu <rikuyu5884@gmail.com>
2024-07-17 16:17:03 +0900

    add berry screen

--- lib/presentation/screen/all_berry/all_berry_screen.dart
+++ lib/presentation/screen/all_berry/all_berry_screen.dart
@@ -1,14 +1,67 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pokedex/foundation/pokedex_color.dart';
+import 'package:pokedex/foundation/utils/constants.dart';
+import 'package:pokedex/foundation/widgets/pokedex_scaffold.dart';
+import 'package:pokedex/presentation/state/all_berry/all_berry_state.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 @RoutePage()
-class AllBerryScreen extends StatelessWidget {
+class AllBerryScreen extends ConsumerWidget {
   const AllBerryScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = L10n.of(context);
+    final berries = ref.watch(allBerryStateProvider);
+    return PokedexScaffold(
+        title: l10n.berryTitle,
+        body: berries.when(
+            loading: () => const Center(child: CircularProgressIndicator(color: PokedexColor.primaryContainer)),
+            error: (_, __) => const Center(child: Icon(Icons.error)),
+            data: (berries) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
+                child: GridView.builder(
+                    itemCount: berries.length,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      childAspectRatio: 1,
+                    ),
+                    itemBuilder: (c, i) {
+                      return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: PokedexColor.black90Alpha),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                    child: CachedNetworkImage(
+                                  imageUrl: Constants.getItemImage(berries[i].name),
+                                  placeholder: (_, __) =>
+                                      const Icon(Icons.question_mark, color: PokedexColor.primaryContainer),
+                                  errorWidget: (_, __, ___) => const Icon(Icons.error),
+                                )),
+                                Text(berries[i].name,
+                                    style: const TextStyle(
+                                      color: PokedexColor.black90Alpha,
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ],
+                            ),
+                          ));
+                    }),
+              );
+            }));
   }
 }
--- lib/presentation/screen/all_pokemon/all_pokemon_screen.dart
+++ lib/presentation/screen/all_pokemon/all_pokemon_screen.dart
@@ -15,36 +15,40 @@ class AllPokemonScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     L10n l10n = L10n.of(context);
-    AllPokemonState notifier = ref.read(allPokemonStateProvider.notifier);
     AsyncValue<List<Pokemon>> state = ref.watch(allPokemonStateProvider);
 
     return PokedexScaffold(
         title: l10n.appTitle,
-        body: Container(
-          color: PokedexColor.backgroundColor,
-          child: CustomScrollView(slivers: [
-            SliverPadding(
-              padding: const EdgeInsets.all(8.0),
-              sliver: SliverGrid(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  mainAxisSpacing: 8,
-                  crossAxisSpacing: 8,
-                  childAspectRatio: 1,
-                ),
-                delegate: SliverChildBuilderDelegate((_, i) {
-                  final pokemon = state.value?[i];
-                  if (pokemon == null) return null;
-                  return AllPokemonItem(pokemon);
-                }, childCount: state.value?.length ?? 0),
-              ),
-            ),
-            const SliverPadding(
-              padding: EdgeInsets.symmetric(vertical: 12.0),
-              sliver: SliverToBoxAdapter(
-                  child: Center(child: CircularProgressIndicator(color: PokedexColor.primaryContainer))),
-            ),
-          ]),
-        ));
+        body: state.when(
+            loading: () => const Center(child: CircularProgressIndicator(color: PokedexColor.primaryContainer)),
+            error: (_, __) => const Center(child: Icon(Icons.error)),
+            data: (pokemons) {
+              return Container(
+                color: PokedexColor.backgroundColor,
+                child: CustomScrollView(slivers: [
+                  SliverPadding(
+                    padding: const EdgeInsets.all(8.0),
+                    sliver: SliverGrid(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 8,
+                        childAspectRatio: 1,
+                      ),
+                      delegate: SliverChildBuilderDelegate((_, i) {
+                        final pokemon = state.value?[i];
+                        if (pokemon == null) return null;
+                        return AllPokemonItem(pokemon);
+                      }, childCount: state.value?.length ?? 0),
+                    ),
+                  ),
+                  const SliverPadding(
+                    padding: EdgeInsets.symmetric(vertical: 12.0),
+                    sliver: SliverToBoxAdapter(
+                        child: Center(child: CircularProgressIndicator(color: PokedexColor.primaryContainer))),
+                  ),
+                ]),
+              );
+            }));
   }
 }
